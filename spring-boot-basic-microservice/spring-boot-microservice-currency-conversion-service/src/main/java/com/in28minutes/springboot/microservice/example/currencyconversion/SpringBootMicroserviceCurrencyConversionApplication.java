288a54e2d573fab966d4040eb836cb3d66e13d58
Ranga Rao Karanam <rangaraokaranam@Rangas-MacBook-Pro.local>
2018-03-19 17:10:55 +0530

    I'm too lazy to put in a comment

--- spring-boot-basic-microservice/spring-boot-microservice-currency-conversion-service/src/main/java/com/in28minutes/springboot/microservice/example/currencyconversion/SpringBootMicroserviceCurrencyConversionApplication.java
+++ spring-boot-basic-microservice/spring-boot-microservice-currency-conversion-service/src/main/java/com/in28minutes/springboot/microservice/example/currencyconversion/SpringBootMicroserviceCurrencyConversionApplication.java
@@ -3,7 +3,7 @@ package com.in28minutes.springboot.microservice.example.currencyconversion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.in28minutes.springboot.microservice.example.currencyconversion")
