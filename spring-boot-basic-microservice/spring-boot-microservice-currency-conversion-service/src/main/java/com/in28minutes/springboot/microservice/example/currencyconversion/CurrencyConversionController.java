3c3e34f246949ebe02624f361c8e30e8bbd3a55e
bsmahi <bs.mahi@gmail.com>
2022-09-18 09:12:46 +0530

    Upgrade to Spring Boot 2.7.3
    Migrated Spring Cloud Version to 2021.0.4

--- spring-boot-basic-microservice/spring-boot-microservice-currency-conversion-service/src/main/java/com/in28minutes/springboot/microservice/example/currencyconversion/CurrencyConversionController.java
+++ spring-boot-basic-microservice/spring-boot-microservice-currency-conversion-service/src/main/java/com/in28minutes/springboot/microservice/example/currencyconversion/CurrencyConversionController.java
@@ -16,39 +16,46 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class CurrencyConversionController {
 
-	private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
-	@Autowired
-	private CurrencyExchangeServiceProxy proxy;
+    @Autowired
+    private CurrencyExchangeServiceProxy proxy;
 
-	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
-	public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to,
-			@PathVariable BigDecimal quantity) {
+    @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversionBean convertCurrency(@PathVariable String from,
+                                                  @PathVariable String to,
+                                                  @PathVariable BigDecimal quantity) {
 
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("from", from);
-		uriVariables.put("to", to);
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
 
-		ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity(
-				"http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class,
-				uriVariables);
+        ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity(
+                "http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class,
+                uriVariables);
 
-		CurrencyConversionBean response = responseEntity.getBody();
+        CurrencyConversionBean response = responseEntity.getBody();
 
-		return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
-				quantity.multiply(response.getConversionMultiple()), response.getPort());
-	}
+        return new CurrencyConversionBean(response.id(), from, to, response.conversionMultiple(), quantity,
+                quantity.multiply(response.conversionMultiple()), response.port());
+    }
 
-	@GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
-	public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to,
-			@PathVariable BigDecimal quantity) {
+    @GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from,
+                                                       @PathVariable String to,
+                                                       @PathVariable BigDecimal quantity) {
 
-		CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
+        CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
 
-		logger.info("{}", response);
+        LOGGER.info("{}", response);
 
-		return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
-				quantity.multiply(response.getConversionMultiple()), response.getPort());
-	}
+        return new CurrencyConversionBean(response.id(),
+                from,
+                to,
+                response.conversionMultiple(),
+                quantity,
+                quantity.multiply(response.conversionMultiple()),
+                response.port());
+    }
 
 }
--- spring-boot-basic-microservice/spring-boot-microservice-forex-service/src/main/java/com/in28minutes/springboot/microservice/example/forex/ExchangeValueRepository.java
+++ spring-boot-basic-microservice/spring-boot-microservice-forex-service/src/main/java/com/in28minutes/springboot/microservice/example/forex/ExchangeValueRepository.java
@@ -1,7 +1,8 @@
 package com.in28minutes.springboot.microservice.example.forex;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ExchangeValueRepository extends 
-    JpaRepository<ExchangeValue, Long>{
-  ExchangeValue findByFromAndTo(String from, String to);
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
+
+    ExchangeValue findByFromAndTo(String from, String to);
 }
