81da9616937c8562a89433c394af4e14974f1da8
bsmahi <bs.mahi@gmail.com>
2023-02-12 13:45:45 +0530

    Upgrade to Spring Boot 3 latest and migrated to OpenAPI

--- spring-boot-2-rest-service-with-swagger/src/main/java/com/in28minutes/springboot/rest/example/swagger/ApiDocumentationConfig.java
+++ spring-boot-2-rest-service-with-swagger/src/main/java/com/in28minutes/springboot/rest/example/swagger/ApiDocumentationConfig.java
@@ -1,30 +1,29 @@
 package com.in28minutes.springboot.rest.example.swagger;
 
-import io.swagger.annotations.Contact;
-import io.swagger.annotations.ExternalDocs;
-import io.swagger.annotations.Info;
-import io.swagger.annotations.License;
-import io.swagger.annotations.SwaggerDefinition;
-
-@SwaggerDefinition(
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+/**
+ * Reference : <a href="https://support.intershop.com/kb/index.php/Display/2914L4">...</a>
+ */
+@OpenAPIDefinition(
         info = @Info(
                 description = "Awesome Resources",
                 version = "V12.0.12",
                 title = "Awesome Resource API",
                 contact = @Contact(
-                   name = "Ranga Karanam", 
-                   email = "ranga.karanam@in28minutes.com", 
-                   url = "http://www.in28minutes.com"
+                        name = "Ranga Karanam",
+                        email = "ranga.karanam@in28minutes.com",
+                        url = "http://www.in28minutes.com"
                 ),
                 license = @License(
-                   name = "Apache 2.0", 
-                   url = "http://www.apache.org/licenses/LICENSE-2.0"
+                        name = "Apache 2.0",
+                        url = "http://www.apache.org/licenses/LICENSE-2.0"
                 )
         ),
-        consumes = {"application/json", "application/xml"},
-        produces = {"application/json", "application/xml"},
-        schemes = {SwaggerDefinition.Scheme.HTTP, SwaggerDefinition.Scheme.HTTPS},
-        externalDocs = @ExternalDocs(value = "Read This For Sure", url = "http://in28minutes.com")
+        servers = {@Server(url = "http://in28minutes.com")}
 )
 public interface ApiDocumentationConfig {
 
