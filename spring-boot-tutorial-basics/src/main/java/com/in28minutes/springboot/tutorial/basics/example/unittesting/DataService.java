1637b6f2aaef2b61f62c744abd49ba409f5e2f97
bsmahi <bs.mahi@gmail.com>
2023-02-12 12:36:12 +0530

    Upgrade to Spring Boot 3 latest

--- spring-boot-tutorial-basics/src/main/java/com/in28minutes/springboot/tutorial/basics/example/unittesting/DataService.java
+++ spring-boot-tutorial-basics/src/main/java/com/in28minutes/springboot/tutorial/basics/example/unittesting/DataService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class DataService {
+
     public int[] retrieveAllData() {
         // Some dummy data
         // Actually this should talk to some database to get all the data
--- spring-boot-tutorial-soap-web-services/src/main/java/com/in28minutes/springboot/soap/web/services/example/WebServiceConfig.java
+++ spring-boot-tutorial-soap-web-services/src/main/java/com/in28minutes/springboot/soap/web/services/example/WebServiceConfig.java
@@ -1,4 +1,5 @@
 package com.in28minutes.springboot.soap.web.services.example;
+
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -14,26 +15,28 @@ import org.springframework.xml.xsd.XsdSchema;
 @Configuration
 public class WebServiceConfig {
 
-	@Bean
-	public ServletRegistrationBean messageDispatcherServlet(ApplicationContext context) {
-		MessageDispatcherServlet messageDispatcherServlet = new MessageDispatcherServlet();
-		messageDispatcherServlet.setApplicationContext(context);
-		messageDispatcherServlet.setTransformWsdlLocations(true);
-		return new ServletRegistrationBean(messageDispatcherServlet, "/ws/*");
-	}
+    @Bean
+    public ServletRegistrationBean<?> messageDispatcherServlet(ApplicationContext context) {
+        MessageDispatcherServlet messageDispatcherServlet = new MessageDispatcherServlet();
+        messageDispatcherServlet.setApplicationContext(context);
+        messageDispatcherServlet.setTransformWsdlLocations(true);
 
-	@Bean(name = "students")
-	public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema studentsSchema) {
-		DefaultWsdl11Definition definition = new DefaultWsdl11Definition();
-		definition.setPortTypeName("StudentPort");
-		definition.setTargetNamespace("http://in28minutes.com/students");
-		definition.setLocationUri("/ws");
-		definition.setSchema(studentsSchema);
-		return definition;
-	}
+        return new ServletRegistrationBean<>(messageDispatcherServlet, "/ws/*");
+    }
 
-	@Bean
-	public XsdSchema studentsSchema() {
-		return new SimpleXsdSchema(new ClassPathResource("student-details.xsd"));
-	}
+    @Bean(name = "students")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema studentsSchema) {
+        DefaultWsdl11Definition definition = new DefaultWsdl11Definition();
+        definition.setPortTypeName("StudentPort");
+        definition.setTargetNamespace("http://in28minutes.com/students");
+        definition.setLocationUri("/ws");
+        definition.setSchema(studentsSchema);
+
+        return definition;
+    }
+
+    @Bean
+    public XsdSchema studentsSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("student-details.xsd"));
+    }
 }
