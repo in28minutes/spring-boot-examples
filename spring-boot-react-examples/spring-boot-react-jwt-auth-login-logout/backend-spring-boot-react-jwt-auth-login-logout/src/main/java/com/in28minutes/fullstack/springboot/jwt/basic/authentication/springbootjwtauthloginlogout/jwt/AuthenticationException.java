efab43cb8967ebcbadddd495f98ebf9839493ed8
bsmahi <bs.mahi@gmail.com>
2023-02-18 08:02:17 +0530

    Upgrade to Spring Boot 3 latest

--- spring-boot-react-examples/spring-boot-react-jwt-auth-login-logout/backend-spring-boot-react-jwt-auth-login-logout/src/main/java/com/in28minutes/fullstack/springboot/jwt/basic/authentication/springbootjwtauthloginlogout/jwt/AuthenticationException.java
+++
@@ -1,8 +0,0 @@
-package com.in28minutes.fullstack.springboot.jwt.basic.authentication.springbootjwtauthloginlogout.jwt;
-
-public class AuthenticationException extends RuntimeException {
-    public AuthenticationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
-
--- spring-boot-react-examples/spring-boot-react-jwt-auth-login-logout/backend-spring-boot-react-jwt-auth-login-logout/src/main/java/com/in28minutes/fullstack/springboot/jwt/basic/authentication/springbootjwtauthloginlogout/jwt/JwtTokenResponse.java
+++ spring-boot-react-examples/spring-boot-react-jwt-auth-login-logout/backend-spring-boot-react-jwt-auth-login-logout/src/main/java/com/in28minutes/fullstack/springboot/jwt/basic/authentication/springbootjwtauthloginlogout/jwt/JwtTokenResponse.java
@@ -1,11 +1,3 @@
 package com.in28minutes.fullstack.springboot.jwt.basic.authentication.springbootjwtauthloginlogout.jwt;
 
-import java.io.Serial;
-import java.io.Serializable;
-
-public record JwtTokenResponse(String token) implements Serializable {
-
-    @Serial
-    private static final long serialVersionUID = 8317676219297719109L;
-
-}
+public record JwtTokenResponse(String token) {}
