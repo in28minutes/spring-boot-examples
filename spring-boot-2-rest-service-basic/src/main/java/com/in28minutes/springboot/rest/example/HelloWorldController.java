ac2e94efe5881bdd6e4d75947091df9c00953d5c
gjpark@airquay.com <gjpark@airquay.com>
2025-07-16 14:18:24 +0900

    Test

--- spring-boot-2-rest-service-basic/src/main/java/com/in28minutes/springboot/rest/example/HelloWorldController.java
+++ spring-boot-2-rest-service-basic/src/main/java/com/in28minutes/springboot/rest/example/HelloWorldController.java
@@ -2,6 +2,8 @@ package com.in28minutes.springboot.rest.example;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.in28minutes.springboot.rest.example.student.Student;
+
 public class HelloWorldController {
     @GetMapping("/hello")
     public String hello() {
@@ -17,4 +19,9 @@ public class HelloWorldController {
     public String breakTime() {
         return "안녕히 계세요, Gwangjin!";
     }
+
+    @GetMapping("/email")
+    public String getEmail(Student user) {
+        return user.getName().toLowerCase(); // user가 null일 가능성 있음!
+    }    
 }
