a9ca4863e28c93cd54468497afa685b8dc0e6fcb
gjpark@airquay.com <gjpark@airquay.com>
2025-07-16 10:47:58 +0900

    Add bye funcion

--- spring-boot-2-rest-service-basic/src/main/java/com/in28minutes/springboot/rest/example/HelloWorldController.java
+++ spring-boot-2-rest-service-basic/src/main/java/com/in28minutes/springboot/rest/example/HelloWorldController.java
@@ -3,8 +3,13 @@ package com.in28minutes.springboot.rest.example;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public class HelloWorldController {
-    @GetMapping("/gwangjin")
-    public String helloGwangjin() {
+    @GetMapping("/hello")
+    public String hello() {
         return "안녕하세요, Gwangjin!";
+    }
+
+    @GetMapping("/bye")
+    public String bye() {
+        return "안녕히 계세요, Gwangjin!";
     }
 }
